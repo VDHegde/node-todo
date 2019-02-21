068676cbd94bffae97d1c7c19a4cc380da126372
Hegde <vinayak.d.hegde@accenture.com>
2019-02-21 20:27:27 +0530

    Todo files

---
+++ server/server.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+
+let app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.status(200).json(doc);
+    }, (e) => {
+        res.status(400).json(e);
+    });
+});
+
+app.get('/todos', () => {
+    Todo.find().then((todos) => {
+        res.status(200).json(todos);
+    }, (e) => {
+        res.status(400).json(e);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
